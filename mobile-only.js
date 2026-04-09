f54f17f1d10c7681016e827fba39a1a7f44e89b0
Vale2859 <valeriomontesano2@gmail.com>
2026-04-09 17:14:52 +0200

    Add files via upload

---
+++ mobile-only.js
@@ -0,0 +1,7 @@
+(function(){
+  var path=(location.pathname.split('/').pop()||'index.html').toLowerCase();
+  document.documentElement.classList.add('mobile-only-mode');
+  document.body && document.body.classList.remove('home-desktop');
+  document.querySelectorAll('.home-desktop-nav,.home-nav-actions').forEach(function(el){el.remove();});
+  document.querySelectorAll('[data-desktop-only], .desktop-only').forEach(function(el){el.remove();});
+})();
