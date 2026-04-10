ffd8f524995e9c1e054be3cb26daafe064a73971
Vale2859 <valeriomontesano2@gmail.com>
2026-04-10 14:28:10 +0200

    Upgrade Firebase SDK and add auth/firestore features
    Updated Firebase SDK imports to version 10.12.2 and added authentication and Firestore functionalities.

--- firebase.js
+++ firebase.js
@@ -1,17 +1,53 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, set, get, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, set, get, remove } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  updateProfile
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB7p69J89r5Kwu2YgawkBb9omjojdM4HTM",
   authDomain: "farmaciamontesano.firebaseapp.com",
-  databaseURL: "https://farmaciamontesano-default-rtdb.firebaseio.com",
   projectId: "farmaciamontesano",
   storageBucket: "farmaciamontesano.firebasestorage.app",
   messagingSenderId: "881461241551",
-  appId: "1:881461241551:web:c74d6ee74cb857fa5179ba"
+  appId: "1:881461241551:web:c74d6ee74cb857fa5179ba",
+  databaseURL: "https://farmaciamontesano-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
 
-export { db, ref, set, get, remove };
+const db = getDatabase(app);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export {
+  app,
+  db,
+  ref,
+  set,
+  get,
+  remove,
+  auth,
+  firestore,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  updateProfile,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc
+};
