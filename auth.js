48029434b3de8cc5ba75d084a4d39cf9c03ff05a
Vale2859 <valeriomontesano2@gmail.com>
2026-04-10 16:27:41 +0200

    Delete auth.js

--- auth.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  const isLogged = localStorage.getItem("farmaciaLoggedIn");
-  const user = localStorage.getItem("farmaciaCurrentUser");
-
-  if (isLogged !== "true" || !user) {
-    window.location.href = "login.html";
-  }
-})();
