e96976dc3517c88d5c1171d95c4c29323ad5fd8a
Vale2859 <valeriomontesano2@gmail.com>
2026-04-14 03:39:29 +0200

    Create config.js

---
+++ functions/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  vapid: {
+    publicKey: "INCOLLA_QUI_LA_TUA_PUBLIC_KEY",
+    privateKey: "INCOLLA_QUI_LA_TUA_PRIVATE_KEY"
+  },
+  app: {
+    baseUrl: "https://vale2859.github.io/Prova-app-firebase"
+  }
+};
