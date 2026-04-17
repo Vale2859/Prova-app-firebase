16114c4ca8c108b8a261aef1838bc527627beb39
Vale2859 <valeriomontesano2@gmail.com>
2026-04-17 16:38:51 +0200

    Delete functions directory

--- functions/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  vapid: {
-    publicKey: "BKsw7JV8wDXVyHhV_Tst8qwThhXsHpXasv2gDViKPUUtbRpBsCcp_wIWsbe0VQl7Ha9WE64hbfZCfvOKqtp5_yk",
-    privateKey: "kBO7VfXDVjgXxubyeD4LLXzhnsVsDtOKpkwvpmvpRFA"
-  },
-  app: {
-    baseUrl: "https://vale2859.github.io/Prova-app-firebase"
-  }
-};
